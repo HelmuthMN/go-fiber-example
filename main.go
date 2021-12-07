ee8df631c3ba7a6fa2a5427860c70911a565e7d4
helmuth <helmuthmullernetto@gmail.com>
2021-12-07 15:24:08 -0300

    fixed imports

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"github.com/HelmuthMN/go-fiber-example/database"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	// Start a new fiber app
 	app := fiber.New()
+
+	// Connection to the Database
+	database.ConnectDB()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		err := c.SendString("Testing air")
